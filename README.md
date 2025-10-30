dc8b50944c08a1170d27e6c56f0847bfb3c3859b
nishi kant kumar <nishikants192@gmail.com>
2025-10-30 21:16:54 +0530

    added new features

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # portfolio
 This is my portfolio.
 Added some animation and started building hamburger.
+
+added functionality in icons.
--- app.js
+++ app.js
@@ -3,3 +3,6 @@ icon.addEventListener("click",()=>{
     let sideMenu=document.querySelector(".sideMenu");
     sideMenu.style.marginleft ="-500px";
 })
+
+let sideOption=document.querySelectorAll(".sideOption");
+for(opt )
