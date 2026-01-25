adedaf4c79efe38bb93be728476b33f47b73ebd6
nishi kant kumar <nishikants192@gmail.com>
2026-01-25 12:14:01 +0530

    Added sideOption-functionality and some styling

--- public/Js/script.js
+++ public/Js/script.js
@@ -4,3 +4,16 @@ new Typed("#type", {
     backSpeed: 60,
     loop: true
 });
+
+let sideMenu=document.querySelector(".sideMenu");
+let hamDiv=document.querySelector(".hamDiv");
+hamDiv.addEventListener("click",()=>{
+    sideMenu.style.marginLeft="0"
+    hamDiv.style.display="none";
+})
+
+let crossSign=document.querySelector(".crossSign");
+crossSign.addEventListener("click",()=>{
+    sideMenu.style.marginLeft="-500px";
+    hamDiv.style.display="block";
+})
