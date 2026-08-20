3e43f09a5c482837f3c94bc3eb5812b9efa7c74e
nishi kant kumar <nishikants192@gmail.com>
2026-08-20 15:36:06 +0530

    feat: add project detail page with image gallery

--- routes/project_route.js
+++ routes/project_route.js
@@ -2,19 +2,28 @@ const express = require("express");
 const router = express.Router();
 const storage = require("../cloudinaryConfig.js");
 const multer = require("multer");
-const upload = multer({storage:storage});
+const upload = multer({ storage: storage });
 const Project = require("../models/project_model.js");
 
 // fetching projects
-router.get("/",  async(req, res) => {
+router.get("/", async (req, res) => {
   try {
     const projects = await Project.find();
-    res.render("Home/home.ejs",{projects});
+    res.render("Home/home.ejs", { projects });
   } catch (err) {
-    console.log(err);
+    res.send(err);
   }
 });
 
+router.get("/particular-project/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const project = await Project.findById(id);
+    res.render("Home/particularProject.ejs", { project });
+  } catch (err) {
+    res.send(err);
+  }
+});
 // Add new project
 // router.get("/add-project", (req, res) => {
 //   res.render("Home/addProject.ejs");
